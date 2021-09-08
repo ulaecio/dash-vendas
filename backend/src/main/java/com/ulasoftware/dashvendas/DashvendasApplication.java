3a4dffc18703360be71770267776255af38508d3
ulaecio <ulaecio@live.com>
2021-09-08 11:44:12 -0300

    Project created

---
+++ backend/src/main/java/com/ulasoftware/dashvendas/DashvendasApplication.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.dashvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ulasoftware/dashvendas/DashvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.dashvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
