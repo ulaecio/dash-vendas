d07637780ac8466f0ac647e7b18440eee4e7d64c
ulaecio <ulaecio@live.com>
2021-09-09 21:58:34 -0300

    Pagination

---
+++ backend/src/main/java/com/ulasoftware/dashvendas/service/SaleService.java
@@ -0,0 +1,31 @@
+package com.ulasoftware.dashvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ulasoftware.dashvendas.dto.SaleDTO;
+import com.ulasoftware.dashvendas.entities.Sale;
+import com.ulasoftware.dashvendas.repositories.SaleRepository;
+import com.ulasoftware.dashvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+		
+	}
+
+}
