fcfb296109b86fb73f52e82eb6181524dcba79da
ulaecio <ulaecio@live.com>
2021-09-13 08:41:51 -0300

    Update data table

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -5,173 +5,173 @@ const DataTable = () => {
           <thead>
               <tr>
                   <th>Rota</th>
-                  <th>Clientes visitados</th>
+                  <th>Quant. clientes visitados</th>
                   <th>Vol. vendido</th>
-                  <th>Valor unit. vendido</th>
+                  <th>Valor vendido</th>
                   <th>Meta</th>
               </tr>
           </thead>
           <tbody>
               <tr>
                   <td>401</td>
-                  <td>Gilberto Silva</td>
+                  <td>15</td>
                   <td>46 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>1017.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 1017.00</td>
               </tr>            
               <tr>
                   <td>402</td>
-                  <td>Francisco</td>
+                  <td>10</td>
                   <td>23 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>15017.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 15017.00</td>
               </tr>            
               <tr>
                   <td>405</td>
-                  <td>Paulo Vitor</td>
+                  <td>12</td>
                   <td>42 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>12456.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 12456.00</td>
               </tr>            
               <tr>
                   <td>406</td>
-                  <td>Moisés</td>
+                  <td>4</td>
                   <td>15 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>854.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 854.00</td>
               </tr>            
               <tr>
                   <td>407</td>
-                  <td>Piter</td>
+                  <td>5</td>
                   <td>10 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>7458.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 7458.00</td>
               </tr>            
               <tr>
                   <td>409</td>
-                  <td>Jonas</td>
+                  <td>7</td>
                   <td>4 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>452.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 452.00</td>
               </tr>            
               <tr>
                   <td>410</td>
-                  <td>Gustavo</td>
+                  <td>13</td>
                   <td>23 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>945.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 945.00</td>
               </tr>            
               <tr>
                   <td>411</td>
-                  <td>Roberta</td>
+                  <td>11</td>
                   <td>52 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>457.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 457.00</td>
               </tr>            
               <tr>
                   <td>412</td>
-                  <td>Gerlane</td>
+                  <td>9</td>
                   <td>3 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>7458.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 7458.00</td>
               </tr>            
               <tr>
                   <td>414</td>
-                  <td>Gessica</td>
+                  <td>16</td>
                   <td>15 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>45612.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 45612.00</td>
               </tr>            
               <tr>
                   <td>415</td>
-                  <td>Gabriel</td>
+                  <td>17</td>
                   <td>42 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>4579.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 4579.00</td>
               </tr>            
               <tr>
                   <td>416</td>
-                  <td>André</td>
+                  <td>2</td>
                   <td>9 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>34568.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 34568.00</td>
               </tr>            
               <tr>
                   <td>418</td>
-                  <td>Pedro</td>
+                  <td>5</td>
                   <td>29 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>854.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 854.00</td>
               </tr>            
               <tr>
                   <td>421</td>
-                  <td>Joao</td>
+                  <td>4</td>
                   <td>32 caixas</td>
-                  <td>R$ 35,00</td>
+                  <td>R$ 2455</td>
                   <td>4587.00</td>
               </tr>            
               <tr>
                   <td>422</td>
-                  <td>Magno</td>
+                  <td>3</td>
                   <td>42 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>65894.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 65894.00</td>
               </tr>            
               <tr>
                   <td>423</td>
-                  <td>Paula</td>
+                  <td>6</td>
                   <td>46 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>4524.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 4524.00</td>
               </tr>            
               <tr>
                   <td>424</td>
-                  <td>Paulina</td>
+                  <td>8</td>
                   <td>49 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>4212.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 4212.00</td>
               </tr>            
               <tr>
                   <td>425</td>
-                  <td>Paula sousa</td>
+                  <td>13</td>
                   <td>439 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>457.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 457.00</td>
               </tr>            
               <tr>
                   <td>426</td>
-                  <td>Marina</td>
+                  <td>14</td>
                   <td>42 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>10222.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 10222.00</td>
               </tr>            
               <tr>
                   <td>430</td>
-                  <td>Vitor</td>
+                  <td>9</td>
                   <td>470 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>14566.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 14566.00</td>
               </tr>            
               <tr>
                   <td>431</td>
-                  <td>lucas</td>
+                  <td>18</td>
                   <td>47 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>13554.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 13554.00</td>
               </tr>            
               <tr>
                   <td>432</td>
-                  <td>Paulo</td>
+                  <td>20</td>
                   <td>19 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>16017.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 16017.00</td>
               </tr>            
               <tr>
                   <td>440</td>
-                  <td>Marcos</td>
+                  <td>2</td>
                   <td>68 caixas</td>
-                  <td>R$ 35,00</td>
-                  <td>1605.00</td>
+                  <td>R$ 2455</td>
+                  <td>R$ 1605.00</td>
               </tr>            
           </tbody>
       </table>
